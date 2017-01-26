9ad9ea3f154b44ce2fa4527cb7c91d9e075f3c26
mohira <m.ohira@nowall.co.jp>
2017-01-26 15:50:21 +0900

    ルーティングの設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+
   devise_for :users
+
+  resources :users, only: [:index, :show] do
+    resources :posts
+  end
+
+  resources :posts, only: [:index, :show]
 
   root 'posts#index'
 end
