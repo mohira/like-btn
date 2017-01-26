31911c5ef8526af366e0f42eadebff2622896279
mohira <m.ohira@nowall.co.jp>
2017-01-26 14:56:13 +0900

    Seedデータ作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.delete_all
+Post.delete_all
+
+3.times do |i|
+  i += 1
+  user = User.create(
+    email: "user#{i}@example.com",
+    password: 'password'
+  )
+
+  3.times do |j|
+    j += 1
+    Post.create(
+      title: "#{user.email}の記事 その#{j}",
+      body: "body#{j} by #{user.email}",
+      user_id: user.id
+    )
+  end
+end
