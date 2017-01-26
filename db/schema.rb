cb6bf7ae264a147a32527581197367dcab5ad06f
mohira <m.ohira@nowall.co.jp>
2017-01-26 14:52:01 +0900

    Postモデル生成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170126054812) do
+ActiveRecord::Schema.define(version: 20170126055053) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title",      null: false
+    t.text     "body",       null: false
+    t.integer  "user_id",    null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
