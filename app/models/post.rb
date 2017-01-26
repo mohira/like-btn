1992714fee4a563b48074e36c222cfc31ae3b45e
mohira <m.ohira@nowall.co.jp>
2017-01-26 15:26:21 +0900

    関連モデルと外部キーと指定する場合の1対多アソシエーションの記述方法

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
+
+  # 上の記述と同じ
+  # belongs_to :user, class_name: :User, foreign_key: :user_id
+
+  # このような書き方もできる(post.user_infoでアクセス)
+  # belongs_to :user_info, class_name: :User, foreign_key: :user_id
 end
